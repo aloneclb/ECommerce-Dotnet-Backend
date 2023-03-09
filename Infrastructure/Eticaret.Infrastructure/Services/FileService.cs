7c12d98a5b0b2738f20845552497615d81c08991
tunahan <tunahancelebi@derinbilgi.com.tr>
2023-03-10 00:10:35 +0300

    Code Refactor for FileService.

--- Infrastructure/Eticaret.Infrastructure/Services/FileService.cs
+++ Infrastructure/Eticaret.Infrastructure/Services/FileService.cs
@@ -19,21 +19,19 @@ public class FileService : IFileService
     private string wwwroot => _webHostEnvironment.WebRootPath;
 
     private const string ImgFolder = "images";
-    // private const string ArticleImagesFolder = "article-images";
     // private const string UserImagesFolder = "user-images";
 
     public async Task<string> UploadAsync(string name, IFormFile imageFile)
     {
-        // var oldFileName = Path.GetFileNameWithoutExtension(imageFile.FileName); // Yüklenen Dosyanın Adı
-        // var newFileName = $"{name}_{Guid.NewGuid().ToString()}{fileExtensions}"; // Guid de kullanılabilir.
-        var fullPath = $"{wwwroot}/{ImgFolder}/example";
+        var pathToSaveFolder = $"{wwwroot}/{ImgFolder}/example";
 
         // Resimlerin Kaydedileceği dosya yok ise oluşturuyoruz.
-        if (!Directory.Exists(fullPath))
-            Directory.CreateDirectory(fullPath);
+        if (!Directory.Exists(pathToSaveFolder))
+            Directory.CreateDirectory(pathToSaveFolder);
+
         var fileExtensions = Path.GetExtension(imageFile.FileName); // Yüklenen Dosyanın Türü
 
-        var pathAndName = await FileRenameAsync(fullPath, name, fileExtensions);
+        var pathAndName = await FileRenameAsync(pathToSaveFolder, name, fileExtensions);
 
         await using var stream = new FileStream(pathAndName, FileMode.Create, FileAccess.Write, FileShare.None,
             1024 * 1024, false);
@@ -45,38 +43,33 @@ public class FileService : IFileService
 
     private async Task<string> FileRenameAsync(string filePath, string fileName, string extension)
     {
-        // Example  wwwroot/example  /  filename   /  extension
-        //          file path           filename      extension
-        
-        var newFileFullPath = await Task.Run<string>(async () =>
+        var newFileFullPath = await Task.Run(async () =>
         {
-            var newFileName = NameChanger.ReplaceInvalidChars(fileName); // urunismi
-            var newFilePath = Path.Combine(filePath, newFileName); // wwwroot/example/urunismi
-            var newFileFullPath = $"{newFilePath}{extension}"; // wwwroot/example/urunismi.jpg
+            var newFileFullPath = await RenameFileName(filePath, fileName, extension); // wwwroot/example/imagename.jpg
             var imageCount = 0;
-            
+
             while (File.Exists(newFileFullPath))
             {
                 imageCount += 1;
-                newFileFullPath = await Examp(filePath, fileName + imageCount.ToString(), extension);
+                newFileFullPath = await RenameFileName(filePath, fileName + imageCount.ToString(), extension);
             }
 
             return newFileFullPath;
         });
-        
+
         return newFileFullPath;
     }
 
-    private async Task<string> Examp(string filePath, string fileName, string extension)
+    private async Task<string> RenameFileName(string filePath, string fileName, string extension)
     {
-        var deneme = await Task.Run<string>(async () =>
+        var fullFilePath = await Task.Run(() =>
         {
             var newFileName = NameChanger.ReplaceInvalidChars(fileName); // urunismi
             var newFilePath = Path.Combine(filePath, newFileName); // wwwroot/example/urunismi
             var newFileFullPath = $"{newFilePath}{extension}"; // wwwroot/example/urunismi.jpg
-            return newFileFullPath;
+            return Task.FromResult(newFileFullPath);
         });
-        return deneme;
+        return fullFilePath;
     }
 
 
