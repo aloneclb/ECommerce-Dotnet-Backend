1774e5fcd31e03e7781daef7075c750804ae64bb
tunahan <tunahancelebi@derinbilgi.com.tr>
2023-03-10 01:11:25 +0300

    Service Registration for repo

--- Infrastructure/ETicaret.Persistence/PersistenceRegistration.cs
+++ Infrastructure/ETicaret.Persistence/PersistenceRegistration.cs
@@ -1,11 +1,13 @@
 ﻿using ETicaret.Application.Repositories.Customer;
 using ETicaret.Application.Repositories.Order;
 using ETicaret.Application.Repositories.Product;
+using ETicaret.Application.Repositories.ProductImages;
 using ETicaret.Persistence.Contexts;
 using ETicaret.Persistence.ExecutionStrategies;
 using ETicaret.Persistence.Repositories.Customer;
 using ETicaret.Persistence.Repositories.Order;
 using ETicaret.Persistence.Repositories.Product;
+using ETicaret.Persistence.Repositories.ProductImage;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -21,9 +23,9 @@ public static class PersistenceRegistration
             {
                 sql.MigrationsAssembly(typeof(ETicaretDbContext).Assembly.FullName);
                 sql.CommandTimeout((int)TimeSpan.FromSeconds(30).TotalSeconds); // Bir executenın Kaç saniye süreceği
-                sql.ExecutionStrategy(dependencies => new ETicaretExecutionStrategy(dependencies, 
-                    maxRetryCount: 10, 
-                    maxRetryDelay: TimeSpan.FromSeconds(30) 
+                sql.ExecutionStrategy(dependencies => new ETicaretExecutionStrategy(dependencies,
+                    maxRetryCount: 10,
+                    maxRetryDelay: TimeSpan.FromSeconds(30)
                 ));
             });
         });
@@ -35,5 +37,7 @@ public static class PersistenceRegistration
         services.AddScoped<IOrderWriteRepository, OrderWriteRepository>();
         services.AddScoped<IProductReadRepository, ProductReadRepository>();
         services.AddScoped<IProductWriteRepository, ProductWriteRepository>();
+        services.AddScoped<IProductImageReadRepository, ProductImageReadRepository>();
+        services.AddScoped<IProductImageWriteRepository, ProductImageWriteRepository>();
     }
 }
