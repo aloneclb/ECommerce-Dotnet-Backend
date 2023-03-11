ecafc6e8c9cbc1d292354eda366b942aa36cc156
tunahan <tunahancelebi@derinbilgi.com.tr>
2023-03-11 13:46:07 +0300

    Interceptor refactor.

--- Infrastructure/ETicaret.Persistence/Contexts/ETicaretDbContext.cs
+++ Infrastructure/ETicaret.Persistence/Contexts/ETicaretDbContext.cs
@@ -18,35 +18,51 @@ public class ETicaretDbContext : DbContext
     public DbSet<Order> Orders { get; set; }
     public DbSet<Customer> Customers { get; set; }
 
-    
+
     // Repo'da kullandığımız fonksiyonu override etmemiz gerek 4 Overloadı var. 4302
     public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
     {
         // ChangeTracker.Entries() -> Tüm izlenen entityleri verir. -> IEnumerable<EntityEntry>
         // ChangeTracker.Entries<T>() -> T tipinde izlenenleri verir. IEnumerable<EntityEntry<T>>
-        var datas = ChangeTracker.Entries<ISoftDelete>();
+        var softEntities = ChangeTracker.Entries<ISoftDelete>();
+        var timeStampEntities = ChangeTracker.Entries<ITimeStamp>();
 
-        foreach (var data in datas)
+        foreach (var data in softEntities)
         {
-            switch(data.State)
+            switch (data.State)
             {
                 case EntityState.Deleted:
                     data.Entity.IsDeleted = true;
                     data.Entity.IsDeletedAt = DateTime.UtcNow;
                     data.Entity.UpdatedAt = DateTime.UtcNow;
                     break;
-                
+
                 case EntityState.Modified:
                     data.Entity.UpdatedAt = DateTime.UtcNow;
                     break;
-                
+
                 case EntityState.Added:
                     data.Entity.CreatedAt = DateTime.UtcNow;
                     data.Entity.UpdatedAt = DateTime.UtcNow;
                     break;
-            };
+            }
         }
-        
+
+        foreach (var entityEntry in timeStampEntities)
+        {
+            switch (entityEntry.State)
+            {
+                case EntityState.Modified:
+                    entityEntry.Entity.UpdatedAt = DateTime.UtcNow;
+                    break;
+
+                case EntityState.Added:
+                    entityEntry.Entity.CreatedAt = DateTime.UtcNow;
+                    entityEntry.Entity.UpdatedAt = DateTime.UtcNow;
+                    break;
+            }
+        }
+
         return base.SaveChangesAsync(cancellationToken);
     }
 }
